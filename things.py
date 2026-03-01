8e11c9326866b14df36c278e296dffe1c0215c51
Nitin Kumar Patel <nitinpatel6875@gmail.com>
2026-03-01 18:45:10 +0530

    Final change

--- things.py
+++ things.py
@@ -6,6 +6,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features=5000, stop_words='english')
 data = pd.read_csv("names.csv")
 vectors = cv.fit_transform(data['tag']).toarray()
-similarity = cosine_similarity(vectors)
+similarity = cosine_similarity(vectors).astype("float32")
 
 

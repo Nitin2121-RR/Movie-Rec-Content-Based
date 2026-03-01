57a74a3301df9aabb73f206a62b451836ff81ec4
Nitin Kumar Patel <nitinpatel6875@gmail.com>
2026-03-01 19:20:56 +0530

    Lets See

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 data = pd.read_csv("names.csv")
 cv = CountVectorizer(max_features=5000, stop_words='english')
-vectors = cv.fit_transform(data['tag']).toarray()
-similarity = cosine_similarity(vectors).astype("float32")
+vectors = cv.fit_transform(data['tag'])
+
 
 
 
 def recommend(movie):
     movie_index = data[data['title'] == movie].index[0]
-    distances = similarity[movie_index]
+    
+    distances = cosine_similarity(
+    vectors[movie_index],
+    vectors
+    ).flatten()
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:11]
 
     recommended_movies = []
--- things.py
+++ things.py
@@ -1,11 +1,3 @@
-import pandas as pd
-import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
 
-cv = CountVectorizer(max_features=5000, stop_words='english')
-data = pd.read_csv("names.csv")
-vectors = cv.fit_transform(data['tag']).toarray()
-similarity = cosine_similarity(vectors).astype("float32") 
 
 
